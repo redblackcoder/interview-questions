40e591e785273cd3e6a1b7dbf4e004050cb5652d
redblackcoder <hjverma@gmail.com>
2017-01-19 22:51:00 -0800

    WIP for role based config overrides

---
+++ ConfigProcessor.java
@@ -0,0 +1,142 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+/**
+ * Process config to generate final config with parameters applied
+ */
+final class ConfigProcessor {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Missing config values. Provide semi-colon separated config values");
+            return;
+        }
+
+        Map<String, String> configProperties = new HashMap<>();
+        for (String config : args[0].split(";")) {
+            String[] kv = config.split(":");
+            configProperties.put(kv[0].trim(), kv[1].trim());
+        };
+        //configProperties.forEach((k,v) -> System.out.println("Key: " + k + " Value: " + v));
+    }
+
+    private static class Parameter {
+        private ParameterType type;
+        private String value;
+
+        private Parameter(ParameterType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        private static Set<Parameter> parseAllKnownParameters(String[] rawParameters) {
+            Set<Parameter> parsedParameters = new HashSet<>();
+            if (rawParameters == null) {
+                return parsedParameters;
+            }
+
+            for (String rawParameter : rawParameters) {
+                String[] pv = rawParameter.split("=");
+                if (pv.length != 2) {
+                    continue;
+                }
+
+                ParameterType type = ParameterType.fromString(pv[0]);
+                if (type == null) {
+                    continue;
+                }
+
+                parsedParameters.add(new Parameter(type, pv[1].trim());
+            }
+
+            return parsedParameters;
+        }
+
+        private static enum ParameterType {
+            Role("role"),
+            Service("service"),
+
+            private String typeString;
+
+            private ParameterType(String typeString) {
+                this.typeString = typeString;
+            }
+
+            private static ParameterType fromString(String typeString) {
+                if (typeString == null) {
+                    return null;
+                }
+
+                typeString = typeString.trim();
+                for (ParameterType pt : ParameterType.values()) {
+                    if (typeString.equalsIgnoreCase(pt.typeString)) {
+                        return pt;
+                    }
+                }
+            }
+        }
+    }
+
+    private static class ParameterizedConfigEntry {
+        private final String key;
+        private final Set<Parameter> parameters;
+        private final String value;
+
+        private ParameterizedConfigEntry(String parameterizedKey, String value) {
+            string[] kps = parameterizedKey.split("?");
+            this.key = kps[0];
+            if (kps.length == 1) {
+                this.parameters = null;
+            } else {
+                String[] rawParameters = kps[1].split("&");
+                this.parameters = Parameter.parseAllKnownParameters(rawParameters);
+            }
+
+            this.value = value;
+        }
+    }
+
+    private static class ParameterizedProperty {
+        private String key;
+        private String defaultValue;
+        private List<ParameterizedConfigEntry> parameterizedConfigEntries;
+
+        private ParameterizedProperty() {}
+
+        private static class ParameterizedPropertyBuilder {
+            private List<ParameterizedConfigEntry> parameterizedConfigEntries;
+
+            private ParameterizedPropertyBuilder() {
+                this.parameterizedConfigEntries = new ArrayList<>();
+            }
+
+            private ParameterizedPropertyBuilder add(String parameterizedKey, String value) {
+                this.parameterizedConfigEntries.add(new ParameterizedConfigEntry(parameterizedKey, value));
+                return this;
+            }
+
+            private ParameterizedProperty build() {
+                ParameterizedProperty pp = new ParameterizedProperty();
+                for (ParameterizedConfigEntry pce : this.parameterizedConfigEntries) {
+                    if (pp.key == null) {
+                        pp.key = pce.key;
+                    }
+
+                    if (pce.parameters == null || pce.parameters.size() == 0) {
+                        pp.defaultValue = pce.value;
+                        break;
+                    }
+                }
+
+                pp.parameterizedConfigEntries = this.parameterizedConfigEntries;
+                return pp;
+            }
+        }
+    }
+}
+
